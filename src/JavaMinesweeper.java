750e5eb650f97c00b221ed3fcaf548e86c02a166
Ruslan Iusupov <frostwow13@yandex.ru>
2023-06-24 23:58:40 +0400

    Add classes: Coord, Ranges, Game

--- src/JavaMinesweeper.java
+++ src/JavaMinesweeper.java
@@ -1,10 +1,14 @@
 import javax.swing.*;
 import java.awt.*;
 import sweeper.Box;
+import sweeper.Coord;
+import sweeper.Game;
+import sweeper.Ranges;
 
 public class JavaMinesweeper extends JFrame {
-    private final int COLS = 15;
-    private final int ROWS = 1;
+    private Game game;
+    private final int COLS = 9;
+    private final int ROWS = 9;
     private final int IMAGE_SIZE = 50;
     private JPanel panel;
 
@@ -13,6 +17,7 @@ public class JavaMinesweeper extends JFrame {
     }
 
     private JavaMinesweeper() {
+        game = new Game(COLS, ROWS);
         setImages();
         initPanel();
         initFrame();
@@ -23,21 +28,21 @@ public class JavaMinesweeper extends JFrame {
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
-                for (Box box : Box.values()) {
-                    g.drawImage((Image) box.image, box.ordinal() * IMAGE_SIZE, 0, this);
+                for (Coord coord : Ranges.getAllCoords()) {
+                    g.drawImage((Image) game.getBox(coord).image, coord.x * IMAGE_SIZE, coord.y * IMAGE_SIZE, this);
                 }
             }
         };
-        panel.setPreferredSize(new Dimension(COLS * IMAGE_SIZE, ROWS * IMAGE_SIZE));
+        panel.setPreferredSize(new Dimension(Ranges.getSize().x * IMAGE_SIZE, Ranges.getSize().y * IMAGE_SIZE));
         add(panel);
     }
 
     private void initFrame() {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("Java Minesweeper");
-        setVisible(true);
         setResizable(false);
         pack();
+        setVisible(true);
         setLocationRelativeTo(null);
     }
 
@@ -45,6 +50,7 @@ public class JavaMinesweeper extends JFrame {
         for (Box box : Box.values()) {
             box.image = getImage(box.name().toLowerCase());
         }
+        setIconImage(getImage("icon"));
     }
 
     private Image getImage(String name) {
